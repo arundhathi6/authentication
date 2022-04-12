2fd4612ee6a1c555dca7872bc3fcb3c822f5bb83
Arundhathi R Menon <43034600+arundhathi6@users.noreply.github.com>
2022-04-12 19:02:30 +0530

    cmt

---
+++ src/model/useModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const bcrypt= require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+    name :{type:String,required:true},
+    email:{type:String,required:true,unique:true},
+    mobile:{type:Number,required:true,unique:true},
+    password:{type:String,required:true,minlength:8}
+},{
+    versionKey:false,
+    timestamps:true
+})
+
+userSchema.pre("save",function(next){
+    const hashPassword = bcrypt.hashSync(this.password,8)
+    this.password=hashPassword;
+    next();
+})
+
+userSchema.methods.checkPassword = function(password){
+    return bcrypt.compareSync(password,this.password);
+}
+const User = mongoose.model("user",userSchema);
+module.exports=User;
